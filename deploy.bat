b4e03e2668d998ad601b9cbb2e4903cf6411c6d7
Eduardo <5426355+emrojo@users.noreply.github.com>
2026-08-27 23:50:40 +0200

    deploy: adjust docker stack deployment commands in deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
 git add .
 git commit -m "fix: new fix"
 git push origin main
-ssh pachas "cd /var/www/pachas && ./deploy.sh"
-ssh pachas "cd /var/www/pachas && ./build.sh"
-ssh pachas "cd /var/www/pachas && ./service.sh restart"
+ssh pachas "cd /var/www/pachas && sudo -u nodeuser git pull origin main"
+ssh pachas "cd /var/www/pachas && sudo -u nodeuser npm build"
+ssh pachas "cd /var/www/pachas && sudo systemctl restart pachas.service"
