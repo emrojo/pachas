bc69cfe58ef4bdf4bbb1e67ca94aac6c54353037
Eduardo <5426355+emrojo@users.noreply.github.com>
2026-08-27 20:00:36 +0200

    deploy: fix batch script syntax and execution flags in deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
 git add .
 git commit -m "fix: new fix"
 git push origin main
-ssh pachas ./deploy.sh
-ssh pachas ./build.sh
-ssh pachas ./service.sh restart
+ssh pachas "cd /var/www/pachas && ./deploy.sh"
+ssh pachas "cd /var/www/pachas && ./build.sh"
+ssh pachas "cd /var/www/pachas && ./service.sh restart"
