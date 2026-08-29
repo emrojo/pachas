612dac0eecabbb133e91291063197c6001e8dcef
Eduardo <5426355+emrojo@users.noreply.github.com>
2026-08-29 11:41:36 +0200

    deploy: fix service restart flow in deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -2,5 +2,6 @@ git add .
 git commit -m "fix: new fix"
 git push origin main
 ssh pachas "cd /var/www/pachas && sudo -u nodeuser git pull origin main"
+ssh pachas "cd /var/www/pachas && sudo -u nodeuser npm install"
 ssh pachas "cd /var/www/pachas && sudo -u nodeuser npm run build"
 ssh pachas "cd /var/www/pachas && sudo systemctl restart pachas.service"
