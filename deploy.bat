ee2571a3a4079bdc0c663c13f3e292711e21a3f4
Eduardo <5426355+emrojo@users.noreply.github.com>
2026-08-30 23:49:02 +0200

    deploy: improve database backup and migration steps in deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -4,4 +4,5 @@ git push origin main
 ssh pachas "cd /var/www/pachas && sudo -u nodeuser git pull origin main"
 ssh pachas "cd /var/www/pachas && sudo -u nodeuser npm install"
 ssh pachas "cd /var/www/pachas && sudo -u nodeuser npm run build"
+ssh pachas "cd /var/www/pachas && sudo -u nodeuser npm run db:migrate"
 ssh pachas "cd /var/www/pachas && sudo systemctl restart pachas.service"
